4f5637a1e5bbc4ba52574f876e6f7969292af799
MUNJEONG KIM <munjeongkim@MUNJEONGui-MacBookAir.local>
2022-06-11 03:14:03 +0900

    coffeegongbang

---
+++ coffeeGongbang/app/src/main/java/com/example/coffeeGongbang/hollys_list.java
@@ -0,0 +1,103 @@
+package com.example.coffeeGongbang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.example.coffeeGongbang.R.layout.list_item;
+
+public class hollys_list extends AppCompatActivity {
+    private ListView list;
+    String[] name_list = new String[91];
+    String[] price_list = new String[91];
+    int [] draw = new int[91];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hollys_list);
+
+        String tmp = makeDataString(R.raw.hollys_menu);
+        makeClass(tmp);
+
+        list = (ListView)findViewById(R.id.hollys);
+
+        CustomList adapter = new CustomList(hollys_list.this);
+        list.setAdapter(adapter);
+    }
+
+    class CustomList extends ArrayAdapter<String>{
+        private final Activity context;
+        public CustomList(Activity context){
+            super(context, R.layout.list_item,name_list);
+            this.context = context;
+        }
+        @Override
+        public View getView(int position, View view, ViewGroup parent){
+            LayoutInflater inflater = context.getLayoutInflater();
+            View rowView = inflater.inflate(list_item, null,true);
+            ImageView image = (ImageView) rowView.findViewById(R.id.image);
+            TextView name = (TextView) rowView.findViewById(R.id.name);
+            TextView price = (TextView) rowView.findViewById(R.id.price);
+
+            image.setImageResource(draw[position]);
+            name.setText(name_list[position]);
+            price.setText(price_list[position]);
+
+            return rowView;
+        }
+
+    }
+
+    public String makeDataString(int menu){
+        InputStream inputStream = getResources().openRawResource(Integer.parseInt(String.valueOf(menu)));
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        int i=0;
+        try {
+            i = inputStream.read();
+            while (i != -1)
+            {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String tmp = byteArrayOutputStream.toString();
+
+        return tmp;
+    }
+
+    public void makeClass(String tmp){
+        String[] arr_t;
+        arr_t = tmp.split("\n");
+
+        int count=0;
+        int n=0;
+        while(n<(arr_t.length)){
+            if(n>=1001) break;
+            name_list[count] = arr_t[n+1];
+            price_list[count] = arr_t[n+4];
+            String a = "hollys"+count;
+            int res = getResources().getIdentifier(a, "drawable", getPackageName());
+            draw[count] = res;
+            n += 11;
+            count++;
+        }
+    }
+}
+
---
+++ coffeeGongbang/app/src/main/java/com/example/coffeeGongbang/startPage.java
@@ -0,0 +1,72 @@
+package com.example.coffeeGongbang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.Serializable;
+
+class coffeeInformation implements Serializable {
+    String number;
+    String name ;
+    String explanation;
+    String size;
+    String price;
+    String calorie;
+    String caffine;
+    String sweet;
+    String saturation;
+    String protein;
+    public String type;
+    String cafeName;
+
+    public coffeeInformation(String a, String b, String c, String d, String e, String f, String g, String h, String i, String j, String k,String l) {
+        number = a;
+        name = b;
+        explanation = c;
+        size = d;
+        price = e;
+        calorie = f;
+        caffine = g;
+        sweet = h;
+        saturation = i;
+        protein = j;
+        type = k;
+        cafeName = l;
+    }
+}
+
+public class startPage extends AppCompatActivity {
+    Button startTest;
+    Button skipTest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+
+        startTest = (Button) findViewById(R.id.startTest);
+        skipTest = (Button) findViewById(R.id.skipTest);
+
+        startTest.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),selectCafe.class);
+                startActivity(intent);
+            }
+        });
+
+        skipTest.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),mainPage.class);
+                intent.putExtra("skip", false);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
